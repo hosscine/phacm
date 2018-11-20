55a505a0ea60b7a79d44b51bdcf62abfc5c9cba2
hosscine <futagamirentaro@gmail.com>
2018-11-20 19:15:09 +0900

    modify count_smooth_maximal as S3method

--- R/count-smooth.R
+++ R/count-smooth.R
@@ -1,59 +1,73 @@
 #' Count local maximal of persistent landscape with smoothing
 #'
-#' @param x `pd` object or `pl` object.
+#' Calculate fuzy betti number of the persistent diagram
+#' like human expert.
+#'
+#' The number of local maximal of persistent landscape is corresponding to
+#' the betti number of the data.
+#' However, the counting result is frequently larger than the true betti number
+#' because of some jaggies on the persistent landscape.
+#' Human experts who analyze persistent landscape calculate the betti number
+#' fuzzly to interpret the jaggies.
+#' `count_smooth_maximal` imitates the method of human expert's analysis
+#' by counting local maximal of multi resolutional smoothed persistent landscape
+#' and meaning it.
+#'
+#' @param x `pd` or `pl` object.
 #' @param ... other arguments passed to specific methods.
 #' @return counting result.
+#' @seealso [count_local_maximal()], [stats::smooth.spline()]
+#' @references R.Futagami, N. Yamada, T. Shibuya.
+#' "Infering Underlying Manifold of Data by the Use of Persistent Homology Analysis."
+#' Proc. of 7th Workshop on Computational Topology in Image Context, The Spain, Jan. 2018.
 #' @export
 count_smooth_maximal <- function(x, ...) {
   UseMethod("count_smooth_maximal")
 }
 
-
-#' Title
+#' Count local maximal of persistent landscape with smoothing
 #'
-#' @param spar
-#' @param plot
-#' @param ...
-#' @param x
-#' @param exist.method
-#' @param cutoff.method
-#'
-#' @return
+#' @param x `pd` object.
+#' @param exist.method the function to compute threshold
+#' for judging whther the cycle exists or not on each dimension.
+#' @param cutoff.method the function to compute threshold
+#' for taking out small persistence holes.
+#' @param spar smoothing parameters to be passed [stats::smooth.spline()].
+#' @param plot if `TRUE`, plot smoothing result.
+#' @param ... ignored.
 #' @export
-#'
-#' @examples
+#' @rdname count_smooth_maximal
 count_smooth_maximal.pd <- function(x,
                                     exist.method = zero_threshold,
-                                    cutoff.method,
+                                    cutoff.method = zero_hat_threshold,
                                     spar = seq(0, 1, 0.1),
                                     plot = TRUE, ...) {
+  assert_that(is.function(exist.method))
+  assert_that(is.function(cutoff.method))
+
   pl <- compute_pl(pd)
-  if (is.null(thresh))
-    thresh <- max(PL)/4
-  estimate <- countSmoothLocalMaximalPL(PL, thresh, spar, plot, ...)
-  return(estimate)
+  count_smooth_maximal(pl,
+                       exist.method = exist.method, cutoff.method = cutoff.method,
+                       spar = spar, plot = plot, ...)
 }
 
-#' Title
+#' Count local maximal of persistent landscape with smoothing
 #'
-#' @param PL
-#' @param dimension
-#' @param thresh
-#' @param spar
-#' @param plot
-#' @param ...
-#'
-#' @return
+#' @param x `pl` object.
+#' @inheritParams count_smooth_maximal.pd
 #' @export
-#'
-#' @examples
-countSmoothLocalMaximalPL <- function(pl, thresh = max(pl)/4, spar = seq(0, 1, 0.1), plot = F, ...) {
-  assert_that()
-  assert_that()
+#' @rdname count_smooth_maximal
+count_smooth_maximal.pl <- function(x,
+                                    exist.method = zero_threshold,
+                                    cutoff.method = zero_hat_threshold,
+                                    spar = seq(0, 1, 0.1),
+                                    plot = TRUE, ...) {
+  assert_that(is.function(exist.method))
+  assert_that(is.function(cutoff.method))
 
-  smPL.list <- lapply(spar, function(sp) stats::smooth.spline(x = 1:length(pl), y = pl, spar = sp))
+  sms.list <- lapply(spar, function(sp) stats::smooth.spline(x = 1:length(pl), y = pl, spar = sp))
 
-  estimate <- smPL.list %>% sapply(countLocalMaximalPL, thresh) %>% mean
+  estimate <- sms.list %>% sapply(countLocalMaximalPL, thresh) %>% mean
   if (!plot)
     return(estimate)
 
@@ -63,8 +77,8 @@ countSmoothLocalMaximalPL <- function(pl, thresh = max(pl)/4, spar = seq(0, 1, 0
   elp <- myfs::softwriteEllipsis(..., append = elp, xlab = "(Birth + Death) / 2", ylab = "(Death - Birth) / 2")
   do.call(graphics::plot, elp)
   graphics::abline(thresh, 0, col = 2)
-  col <- smPL.list %>% length %>% grDevices::rainbow()
-  for (i in 1:length(smPL.list)) graphics::lines(smPL.list[[i]]$y, col = col[i])
+  col <- sms.list %>% length %>% grDevices::rainbow()
+  for (i in 1:length(sms.list)) graphics::lines(sms.list[[i]]$y, col = col[i])
 
   return(estimate)
 }
--- R/threshold.R
+++ R/threshold.R
@@ -27,3 +27,33 @@ zero_threshold.pd <- function(x) {
 zero_threshold.pl <- function(x) {
   attr(x, "pd") %>% zero_threshold
 }
+
+#' Cmmpute mean of non 0-dimensional persistence
+#'
+#' The 0-dimensional persistence is one of the indicators
+#' for the connectedness of data.
+#' We can use the value for thresholding non 0-dimensional persistence
+#' to judge whether the cycle is exist or not on each dimension.
+#'
+#' @param x `pd` or `pl` object.
+#' @return mean of 0-dimensional persistence.
+#' @export
+zero_hat_threshold <- function(x) {
+  UseMethod("zero_hat_threshold")
+}
+
+#' @export
+#' @rdname zero_hat_threshold
+zero_hat_threshold.pd <- function(x) {
+  x %>%
+    filter(dim != 0) %>%
+    mutate(persistence = death - birth) %>%
+    use_series(persistence) %>%
+    mean
+}
+
+#' @export
+#' @rdname zero_hat_threshold
+zero_hat_threshold.pl <- function(x) {
+  attr(x, "pd") %>% zero_hat_threshold
+}
